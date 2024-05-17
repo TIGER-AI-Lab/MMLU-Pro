27bf42c711f0f2766a1f753efa210a192df165f0
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-17 18:21:59 +0800

    upload api eval code

---
+++ evaluate_from_api.py
@@ -0,0 +1,219 @@
+import os
+from openai import AzureOpenAI
+import json
+import re
+import random
+from tqdm import tqdm
+import time
+from datasets import load_dataset
+
+API_KEY = '786f8c1ff30a4c4b9f9b8917f2f5191b'
+
+my_client = AzureOpenAI(
+  azure_endpoint="https://waterloo-gpt4.openai.azure.com/",
+  api_key=API_KEY,
+  api_version="2024-02-15-preview"
+)
+
+
+def call_gpt_4(client, instruction, inputs):
+    start = time.time()
+    message_text = [{"role": "user", "content": instruction + inputs}]
+    completion = client.chat.completions.create(
+      model="gpt-4",
+      messages=message_text,
+      temperature=0,
+      max_tokens=4000,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0,
+      stop=None
+    )
+    print("cost time", time.time() - start)
+    return completion.choices[0].message.content
+
+
+def load_mmlu_pro():
+    dataset = load_dataset("TIGER-Lab/MMLU-Pro")
+    test_df, val_df = dataset["test"], dataset["validation"]
+    test_df = preprocess(test_df)
+    val_df = preprocess(val_df)
+    return test_df, val_df
+
+
+def preprocess(test_df):
+    res_df = []
+    for each in test_df:
+        options = []
+        for opt in each["options"]:
+            if opt == "N/A":
+                continue
+            options.append(opt)
+        each["options"] = options
+        res_df.append(each)
+    res = {}
+    for each in res_df:
+        if each["category"] not in res:
+            res[each["category"]] = []
+        res[each["category"]].append(each)
+    return res
+
+
+def format_example(question, options, cot_content=""):
+    if cot_content == "":
+        cot_content = "Let think step by step."
+    if cot_content.startswith("A: "):
+        cot_content = cot_content[3:]
+    example = "Question: {}\nOptions: ".format(question)
+    choice_map = "ABCDEFGHIJ"
+    for i, opt in enumerate(options):
+        example += "{}. {}\n".format(choice_map[i], opt)
+    if cot_content == "":
+        example += "Answer: "
+    else:
+        example += "Answer: " + cot_content + "\n\n"
+    return example
+
+
+def extract_answer(text):
+    pattern = r"answer is \(?([ABCDEFGHIJ])\)?"
+    match = re.search(pattern, text)
+    if match:
+        return match.group(1)
+    else:
+        print("extraction failed:\n", text)
+        return None
+
+
+def single_request_gpt4(single_question, cot_examples_dict, exist_result):
+    exist = True
+    q_id = single_question["question_id"]
+    for each in exist_result:
+        if q_id == each["question_id"] and single_question["question"] == each["question"]:
+            print("already exists, skip it")
+            return each["pred"], each["gpt_4_response"], exist
+    exist = False
+    category = single_question["category"]
+    cot_examples = cot_examples_dict[category]
+    question = single_question["question"]
+    options = single_question["options"]
+    prompt = "The following are multiple choice questions (with answers) about {}. Think step by" \
+             " step and then output the answer in the format of \"The answer is (X)\" at the end.\n\n" \
+        .format(category)
+    for each in cot_examples:
+        prompt += format_example(each["question"], each["options"], each["cot_content"])
+    input_text = format_example(question, options)
+    try:
+        start = time.time()
+        response = call_gpt_4(my_client, prompt, input_text)
+        print("requesting gpt 4 costs: ", time.time() - start)
+    except Exception as e:
+        print("error", e)
+        return None, None, exist
+    pred = extract_answer(response)
+    return pred, response, exist
+
+
+def update_result(output_res_path):
+    category_record = {}
+    res = []
+    success = False
+    while not success:
+        try:
+            if os.path.exists(output_res_path):
+                with open(output_res_path, "r") as fi:
+                    res = json.load(fi)
+                    for each in res:
+                        category = each["category"]
+                        if category not in category_record:
+                            category_record[category] = {"corr": 0.0, "wrong": 0.0}
+                        if not each["pred"]:
+                            random.seed(12345)
+                            x = random.randint(0, len(each["options"]) - 1)
+                            if x == each["answer_index"]:
+                                category_record[category]["corr"] += 1
+                                print("random hit.")
+                            else:
+                                category_record[category]["wrong"] += 1
+                        elif each["pred"] == each["answer"]:
+                            category_record[category]["corr"] += 1
+                        else:
+                            category_record[category]["wrong"] += 1
+            success = True
+        except Exception as e:
+            print("Error", e, "sleep 2 seconds")
+            time.sleep(2)
+    return res, category_record
+
+
+def evaluate():
+    test_df, dev_df = load_mmlu_pro()
+    for subject in assigned_subject:
+        test_data = test_df[subject]
+        output_res_path = os.path.join(output_dir, subject + "_result.json")
+        output_summary_path = os.path.join(output_dir, subject + "_summary.json")
+        res, category_record = update_result(output_res_path)
+
+        for each in tqdm(test_data):
+            label = each["answer"]
+            category = subject
+            pred, response, exist = single_request_gpt4(each, dev_df, res)
+            if exist:
+                continue
+            if response is not None:
+                res, category_record = update_result(output_res_path)
+                if category not in category_record:
+                    category_record[category] = {"corr": 0.0, "wrong": 0.0}
+                each["pred"] = pred
+                each["gpt_4_response"] = response
+                res.append(each)
+                if pred is not None:
+                    if pred == label:
+                        category_record[category]["corr"] += 1
+                    else:
+                        category_record[category]["wrong"] += 1
+                else:
+                    category_record[category]["wrong"] += 1
+                save_res(res, output_res_path)
+                save_summary(category_record, output_summary_path)
+                res, category_record = update_result(output_res_path)
+        save_res(res, output_res_path)
+        save_summary(category_record, output_summary_path)
+
+
+def save_res(res, output_res_path):
+    temp = []
+    exist_q_id = []
+    for each in res:
+        if each["question_id"] not in exist_q_id:
+            exist_q_id.append(each["question_id"])
+            temp.append(each)
+        else:
+            continue
+    res = temp
+    with open(output_res_path, "w") as fo:
+        fo.write(json.dumps(res))
+
+
+def save_summary(category_record, output_summary_path):
+    total_corr = 0.0
+    total_wrong = 0.0
+    for k, v in category_record.items():
+        if k == "total":
+            continue
+        cat_acc = v["corr"] / (v["corr"] + v["wrong"])
+        category_record[k]["acc"] = cat_acc
+        total_corr += v["corr"]
+        total_wrong += v["wrong"]
+    acc = total_corr / (total_corr + total_wrong)
+    category_record["total"] = {"corr": total_corr, "wrong": total_wrong, "acc": acc}
+    with open(output_summary_path, "w") as fo:
+        fo.write(json.dumps(category_record))
+
+
+if __name__ == '__main__':
+    assigned_subject = []
+    output_dir = "eval_results/gpt-4-results_from_api"
+    os.makedirs(output_dir, exist_ok=True)
+    evaluate()
+
