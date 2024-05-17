9f6bb56bca66e1d649cb29d814cb95009a948b76
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-17 17:55:11 +0800

    update

--- scripts/examples/eval_llama_2_7b.sh
+++ scripts/examples/eval_llama_2_7b.sh
@@ -5,6 +5,7 @@ save_dir="eval_results/"
 global_record_file="eval_results/eval_record_collection_0514_darth.csv"
 model="meta-llama/Llama-2-7b-hf"
 selected_subjects="all"
+gpu_util=0.6
 
 cd ../../
 export CUDA_VISIBLE_DEVICES=0
@@ -14,7 +15,9 @@ python evaluate_from_local.py \
                  --ngpu $ngpu \
                  --save_dir $save_dir \
                  --model $model \
-                 --global_record_file $global_record_file
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util
+
 
 
 
